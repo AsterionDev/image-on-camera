83a22a6f07ec307ac8a7f42ac76c8709e2ea1e15
David Sotelo <jsotelo@yeapp.io>
2025-04-26 12:24:06 -0500

    update: README

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import fs from 'fs';
 import path from 'path';
 
 export default defineConfig({
+  base: '/image-on-camera/',
   plugins: [preact()],
-  server: {
-    host: true,      // para exponer en tu IP local
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/127.0.0.1+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/127.0.0.1+2.pem')),
-    },
-  },
+  // server: {
+  //   host: true,      // para exponer en tu IP local
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(__dirname, 'certs/127.0.0.1+2-key.pem')),
+  //     cert: fs.readFileSync(path.resolve(__dirname, 'certs/127.0.0.1+2.pem')),
+  //   },
+  // },
 });
 
